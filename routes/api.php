b760cb275533307469772d0e84fcd272e346e97a
mehens1 <mehens1@gmail.com>
2025-08-03 04:14:24 +0100

    cleanup

--- routes/api.php
+++ routes/api.php
@@ -5,18 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WaitlistController;
 
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-require __DIR__.'/v1/v1.php';
+require __DIR__ . '/v1/v1.php';
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
